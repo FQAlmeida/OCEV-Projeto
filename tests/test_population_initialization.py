cca821506aa7cf2fe44a90cb55ac4a1dff751ea8
Otávio Almeida <otavioalmeida650@gmail.com>
2024-03-15 13:50:37 -0300

    reafactor to use classes

--- tests/test_population_initialization.py
+++ tests/test_population_initialization.py
@@ -1,7 +1,14 @@
+from time import time
+
 import numpy as np
 import pytest
+from matplotlib.pylab import SeedSequence
 
-from ocev_projeto.population_initialization import Configs, PopType, generate_pop
+from ocev_projeto.models.config import BoundConfig, PopConfig, PopType
+from ocev_projeto.population_initialization import PopGenerator
+
+seed_seq = SeedSequence(round(time()))
+rng = np.random.default_rng(seed_seq)
 
 
 def test_generate_pop_int_with_config():
@@ -9,25 +16,25 @@ def test_generate_pop_int_with_config():
     pop_size = 60
     upper = 10
     lower = 0
-    config = Configs(upper, lower)
-    result = generate_pop(dim, pop_size, PopType.INT, config)
+    pop_config = PopConfig(
+        dim=dim, pop_size=pop_size, bounds=BoundConfig(lower=lower, upper=upper), pop_type=PopType.INT
+    )
+    pop_gen = PopGenerator(pop_config, rng)
+    result = pop_gen.generate_pop()
     assert isinstance(result, np.ndarray)
     assert result.shape == (pop_size, dim)
     assert np.all(result <= upper)
     assert np.all(result >= lower)
 
 
-def test_generate_pop_permint_with_config():
-    dim = 80
-    pop_size = 70
-    upper = 100
-    lower = 0
-    config = Configs(upper, lower)
-    result = generate_pop(dim, pop_size, PopType.PERMINT, config)
+def test_generate_pop_permint_without_config():
+    dim = 6
+    pop_size = 10
+    pop_config = PopConfig(dim=dim, pop_size=pop_size, pop_type=PopType.PERMINT)
+    pop_gen = PopGenerator(pop_config, rng)
+    result = pop_gen.generate_pop()
     assert isinstance(result, np.ndarray)
     assert result.shape == (pop_size, dim)
-    assert np.all(result <= upper)
-    assert np.all(result >= lower)
     assert np.all(np.apply_along_axis(lambda x: len(np.unique(x)) == len(x), axis=1, arr=result))
 
 
@@ -36,8 +43,11 @@ def test_generate_pop_real_with_config():
     pop_size = 50
     upper = 10
     lower = -10
-    config = Configs(upper, lower)
-    result = generate_pop(dim, pop_size, PopType.REAL, config)
+    pop_config = PopConfig(
+        dim=dim, pop_size=pop_size, bounds=BoundConfig(lower=lower, upper=upper), pop_type=PopType.REAL
+    )
+    pop_gen = PopGenerator(pop_config, rng)
+    result = pop_gen.generate_pop()
     assert isinstance(result, np.ndarray)
     assert result.shape == (pop_size, dim)
     assert np.all(result <= upper)
@@ -47,7 +57,9 @@ def test_generate_pop_real_with_config():
 def test_generate_pop_binary_without_config():
     dim = 100
     pop_size = 150
-    result = generate_pop(dim, pop_size, PopType.BINARY, None)
+    pop_config = PopConfig(dim=dim, pop_size=pop_size, pop_type=PopType.BINARY)
+    pop_gen = PopGenerator(pop_config, rng)
+    result = pop_gen.generate_pop()
     assert isinstance(result, np.ndarray)
     assert np.all(np.logical_or(result == 0, result == 1))
 
@@ -57,7 +69,6 @@ def test_generate_pop_raises_exception_without_config_for_some_types():
     pop_size = 150
     types_that_should_raise_exception = [
         PopType.INT,
-        PopType.PERMINT,
         PopType.REAL,
     ]
     types_that_should_not_raise_exception = [
@@ -65,7 +76,11 @@ def test_generate_pop_raises_exception_without_config_for_some_types():
     ]
     for pop_type in types_that_should_raise_exception:
         with pytest.raises(Exception):
-            generate_pop(dim, pop_size, pop_type, None)
+            pop_config = PopConfig(dim=dim, pop_size=pop_size, pop_type=pop_type)
+            pop_gen = PopGenerator(pop_config, rng)
+            pop_gen.generate_pop()
 
     for pop_type in types_that_should_not_raise_exception:
-        generate_pop(dim, pop_size, pop_type, None)
+        pop_config = PopConfig(dim=dim, pop_size=pop_size, pop_type=pop_type)
+        pop_gen = PopGenerator(pop_config, rng)
+        pop_gen.generate_pop()
