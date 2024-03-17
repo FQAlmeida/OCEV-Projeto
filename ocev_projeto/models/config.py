e8209c6cfba0997dcb789147fcf997d4b1533b38
Otávio Almeida <otavioalmeida650@gmail.com>
2024-03-16 22:23:56 -0300

    pkl config

--- ocev_projeto/models/config.py
+++ ocev_projeto/models/config.py
@@ -1,5 +1,13 @@
 from dataclasses import dataclass
 from enum import Enum
+from pathlib import Path
+
+import pkll
+
+
+class InvalidPopTypeError(Exception):
+    def __init__(self, name: str):
+        super().__init__(f"Invalid PopType {name}")
 
 
 class PopType(Enum):
@@ -7,6 +15,19 @@ class PopType(Enum):
     PERMINT = "permint"
     REAL = "real"
     BINARY = "binary"
+
+    @staticmethod
+    def str_to_type(name: str):
+        match name:
+            case "int":
+                return PopType.INT
+            case "permint":
+                return PopType.PERMINT
+            case "real":
+                return PopType.REAL
+            case "binary":
+                return PopType.BINARY
+        raise InvalidPopTypeError(name)
 
 
 @dataclass
@@ -34,3 +55,19 @@ class Config:
     qtd_gen: int = 1
     qtd_runs: int = 1
     elitism: bool = False
+
+
+def pkl_to_config(pkl_path: str) -> Config:
+    config_path = Path(pkl_path)
+    pkl_config = pkll.load(f"file:///{config_path.absolute()}")
+    config: Config = Config(
+        pop_config=PopConfig(
+            dim=pkl_config.config.pop_config.dim,
+            pop_size=pkl_config.config.pop_config.pop_size,
+            pop_type=PopType.str_to_type(pkl_config.config.pop_config.pop_type),
+        ),
+        qtd_gen=pkl_config.config.qtd_gen,
+        qtd_runs=pkl_config.config.qtd_runs,
+        elitism=pkl_config.config.elitism,
+    )
+    return config
--- ocev_projeto/sat3.py
+++ ocev_projeto/sat3.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import numpy as np
 
 from ocev_projeto.ga import GA
-from ocev_projeto.models.config import Config, PopConfig, PopType
+from ocev_projeto.models.config import pkl_to_config
 
 
 def read_instance(name: str):
@@ -19,28 +19,27 @@ def read_instance(name: str):
 
 
 def objective(problem: np.ndarray, individual: np.ndarray):
-    def evaliate(p):
-        xs = list(map(int, p))
-        xs_abs = map(lambda x: abs(x) - 1, xs)
-        xs_neg = list(map(lambda x: x < 0, xs))
-        xs_bool = map(lambda x: bool(individual[x]), xs_abs)
-        xs_bool = map(lambda x: (x[1] and not xs_neg[x[0]]) or (not x[1] and xs_neg[x[0]]), enumerate(xs_bool))
+    def evaluate(p):
+        xs_abs = np.abs(p) - 1
+        xs_neg = p < 0
+        xs_bool = np.apply_along_axis(lambda x: individual[x], 0, xs_abs)
+        xs_bool = map(
+            lambda x: (x[1] and not xs_neg[x[0]]) or (not x[1] and xs_neg[x[0]]),
+            enumerate(xs_bool),
+        )
         return any(xs_bool) is False
-    solution = np.apply_along_axis(evaliate, 1, problem)
+
+    solution = np.apply_along_axis(evaluate, 1, problem)
     return np.count_nonzero(solution)
 
 
 if __name__ == "__main__":
     config_line, problem, expected_solution = read_instance("uf100-01.cnf")
-    config: Config = Config(
-        pop_config=PopConfig(dim=int(config_line.split(" ")[2]), pop_size=300, pop_type=PopType.BINARY),
-        qtd_gen=1000,
-        qtd_runs=10,
-        elitism=False,
-    )
+    config = pkl_to_config("data/config/sat3.pkl")
     pool = Pool(cpu_count())
     indiv, result = (None, None)
-    for _ in range(config.qtd_runs):
+    for run in range(config.qtd_runs):
+        print(f"Run {run + 1}")
         ga = GA(config, objective, problem, pool)
         new_indiv, new_result = ga.run()
         if not result or new_result < result:
