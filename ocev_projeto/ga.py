c4ba0e1a9740254ae1c813b930e087a648af3b90
Otávio Almeida <otavioalmeida650@gmail.com>
2024-03-25 00:37:56 -0300

    evo operators

--- ocev_projeto/ga.py
+++ ocev_projeto/ga.py
@@ -1,11 +1,15 @@
+import logging
 from multiprocessing.pool import Pool
 from time import time
 
 import numpy as np
 from matplotlib.pylab import SeedSequence
 
+from ocev_projeto.models.config import CrossOverMethod, PopType
 from ocev_projeto.population_initialization import PopGenerator
 from ocev_projeto.problem import Problem
+
+logger = logging.getLogger("GA")
 
 
 class GA:
@@ -15,16 +19,16 @@ class GA:
         pool: Pool,
     ) -> None:
         seed_seq = SeedSequence(round(time()))
-        rng = np.random.default_rng(seed_seq)
+        self.rng = np.random.default_rng(seed_seq)
         self.problem = problem
-        self.pop_generator = PopGenerator(self.problem.config.pop_config, rng)
+        self.pop_generator = PopGenerator(self.problem.config.pop_config, self.rng)
         self.population = self.pop_generator.generate_pop()
         self.best_individual_index = (None, None)
         self.pool = pool
 
     def run(self):
         for _ in range(self.problem.config.qtd_gen):
-            result = self.__fitness(self.population)
+            result = self.__fitness()
             ranked_results = sorted(enumerate(result), key=lambda x: x[1])
             best_individual_index = ranked_results[0]
             if (
@@ -35,12 +39,74 @@ class GA:
                     self.population[best_individual_index[0]],
                     best_individual_index[1],
                 )
-            self.population = self.pop_generator.generate_pop()
+            logger.info(
+                "Best Individual: "
+                f"{self.best_individual} {self.best_individual_value}"
+            )
+            mating_pool = self.__selection(result)
+            mating_pool = self.__cross_over(mating_pool)
+            mating_pool = self.__mutation(mating_pool)
+            self.population = mating_pool
         return self.best_individual, self.best_individual_value
 
-    def __fitness(self, population: np.ndarray):
-        result = self.pool.map(self.problem.objective, population)
+    def __fitness(self):
+        result = self.pool.map(self.problem.objective, self.population)
         return np.array(result)
+
+    def __selection(self, result: np.ndarray):
+        sum_fitness = np.sum(result)
+        p_selection = result / sum_fitness
+        mating_pool = self.rng.choice(
+            len(self.population), len(self.population), True, p_selection
+        )
+        return self.population[mating_pool]
+
+    def __cross_over(self, mating_pool):
+        crossover_method = self.problem.config.crossover_method
+        match crossover_method:
+            case CrossOverMethod.ONE_POINT:
+                return self.__cross_over_one_point(mating_pool)
+        raise Exception("no cross impld")
+
+    def __cross_over_one_point(self, mating_pool: np.ndarray):
+        pop_type = self.problem.config.pop_config.pop_type
+        pop_size = self.problem.config.pop_config.pop_size
+        dim = self.problem.config.pop_config.dim
+        crossover_chance = self.problem.config.crossover_chance
+        match pop_type:
+            case PopType.BINARY:
+                mating_pool = mating_pool.reshape(-1, 2, dim)
+                mask = self.rng.random((pop_size // 2, dim)) < crossover_chance
+                cut = self.rng.choice(dim, (pop_size // 2, 1))
+                mask = np.concatenate(
+                    (np.arange(dim).reshape(1, -1) < cut, mask), axis=1
+                )
+                mating_pool = np.where(
+                    mask, mating_pool, np.roll(mating_pool, 1, axis=1)
+                )
+                mating_pool = mating_pool.reshape(-1, dim)
+                return mating_pool
+        raise Exception("no cross type impld")
+
+    def __mutation(self, mating_pool: np.ndarray):
+        pop_type = self.problem.config.pop_config.pop_type
+        pop_size = self.problem.config.pop_config.pop_size
+        dim = self.problem.config.pop_config.dim
+        mutation_chance = self.problem.config.mutation_chance
+        match pop_type:
+            case PopType.BINARY:
+                mask = self.rng.choice(
+                    [True, False],
+                    (pop_size, dim),
+                    True,
+                    [mutation_chance, 1 - mutation_chance],
+                )
+                mating_pool = np.logical_or(
+                    np.logical_and(mask, np.logical_not(mating_pool)),
+                    np.logical_and(np.logical_not(mask), mating_pool),
+                )
+                return mating_pool
+        raise Exception("no mutation type impld")
 
     @property
     def best_individual(self):
--- ocev_projeto/models/config.py
+++ ocev_projeto/models/config.py
@@ -12,6 +12,16 @@ class InvalidPopTypeError(Exception):
         super().__init__(f"Invalid PopType {name}")
 
 
+class InvalidSelectionMethodError(Exception):
+    def __init__(self, name: str):
+        super().__init__(f"Invalid Selection Method {name}")
+
+
+class InvalidCrossOverMethodError(Exception):
+    def __init__(self, name: str):
+        super().__init__(f"Invalid CrossOver Method {name}")
+
+
 class PopType(Enum):
     INT = "int"
     PERMINT = "permint"
@@ -30,6 +40,31 @@ class PopType(Enum):
             case "binary":
                 return PopType.BINARY
         raise InvalidPopTypeError(name)
+
+
+class SelectionMethod(Enum):
+    ROULETTE = "roulette"
+    TOURNAMENT = "tournament"
+
+    @staticmethod
+    def str_to_type(name: str):
+        match name:
+            case "roulette":
+                return SelectionMethod.ROULETTE
+            case "tournament":
+                return SelectionMethod.TOURNAMENT
+        raise InvalidSelectionMethodError(name)
+
+
+class CrossOverMethod(Enum):
+    ONE_POINT = "one-point"
+
+    @staticmethod
+    def str_to_type(name: str):
+        match name:
+            case "one-point":
+                return CrossOverMethod.ONE_POINT
+        raise InvalidCrossOverMethodError(name)
 
 
 @dataclass
@@ -57,6 +92,10 @@ class Config:
     qtd_gen: int = 1
     qtd_runs: int = 1
     elitism: bool = False
+    selection_method: SelectionMethod = SelectionMethod.ROULETTE
+    crossover_method: CrossOverMethod = CrossOverMethod.ONE_POINT
+    crossover_chance: float = 1.0
+    mutation_chance: float = 0.05
 
 
 def pkl_to_config(pkl_path: str) -> Config:
@@ -70,6 +109,14 @@ def pkl_to_config(pkl_path: str) -> Config:
             pop_size=pkl_config.config.pop_config.pop_size,
             pop_type=PopType.str_to_type(pkl_config.config.pop_config.pop_type),
         ),
+        selection_method=SelectionMethod.str_to_type(
+            pkl_config.config.selection_method
+        ),
+        crossover_method=CrossOverMethod.str_to_type(
+            pkl_config.config.crossover_method
+        ),
+        crossover_chance=pkl_config.config.crossover_chance,
+        mutation_chance=pkl_config.config.mutation_chance,
         qtd_gen=pkl_config.config.qtd_gen,
         qtd_runs=pkl_config.config.qtd_runs,
         elitism=pkl_config.config.elitism,
